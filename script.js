b8f4c5717f044b44bb8dd51f6942f4fb113bf60a
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2023-12-23 19:49:29 +0530

    Add displayMovements

--- script.js
+++ script.js
@@ -61,3 +61,24 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // =====================================================================
+
+const displayMovements = function (movements) {
+	// reset the container first
+	containerMovements.innerHTML = '';
+
+	movements.forEach(function (mov, i) {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+		const html = `
+        <div class="movements__row">
+            <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+            <!-- <div class="movements__date">3 days ago</div> -->
+            <div class="movements__value">${mov}</div>
+        </div>`;
+
+		// notice insertAdjacentHTML and not insertAdjacentElement
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
+};
+
+displayMovements(account1.movements);
